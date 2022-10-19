07c8f0feebfa6073e52300172ec96198884ee02e
Andrey <bakugan_a2002@mail.ru>
2022-10-19 20:37:42 +0300

    Доверстал 1 страницу, создал роуты, декомпозировал инпут и хедер, добавил к ним scss модули, добавил страницу Тезауруса

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
-import "./styles/App.scss";
+import "./App.scss";
 import React from "react";
+import Start from "./pages/Start/Start";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Thesaurus from "./pages/Thesaurus/Thesaurus";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="header__logo">
-          <img src={require("./img/bmstu.png")} alt="" />
-          <div className={"header__name"}>
-            <p>МГТУ им. Н.Э Баумана</p>
-            <p>Создаем инженеров</p>
-          </div>
-        </div>
-        <div className="header__menu">
-          <p>Тезаурус</p>
-          <p>Рус</p>
-          <p>Eng</p>
-        </div>
-      </div>
-      <div className="wrapper">
-        <div className="container">
-          <h1>База и генератор образовательных ресурсов</h1>
-          <div className="search">
-            <div className="search__icon"></div>
-            <div className="search__vert"></div>
-            <div className="search__arr"></div>
-            <span className="search__menu">Курсы</span>
-            <input
-              className="search__input"
-              type="text"
-              placeholder="Поиск..."
-            />
-          </div>
-          <div className="transition">
-            <span>Перейти ко всем учебным курсам</span>
-            <div className="transition__arr"></div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/Thesaurus" element={<Thesaurus />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Thesaurus/Thesaurus.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Thesaurus = () => {
+  return <div>hello</div>;
+};
+
+export default Thesaurus;
