005c577f5b748c2584f473370d5edb3b15c823e7
andreyivashchenko <99690245+andreyivashchenko@users.noreply.github.com>
2022-10-29 17:30:52 +0300

    Причесал почти всю верстку главной страницы

---
+++ src/pages/Start/Start.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Start.scss";
+import Input from "../../components/Input/Input";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Start = () => {
+  return (
+    <div className="wrapper">
+      <div className="wrapper__home">
+        <Header />
+        <div className="wrapper__home__container">
+          <h1>База и Генератор Образовательных Ресурсов</h1>
+          <Input />
+          <div className="transition">
+            <span>Перейти ко всем учебным курсам</span>
+            <a href="#courses" className="transition__arr"></a>
+          </div>
+        </div>
+      </div>
+      <div id="courses" className="wrapper__courses">
+        <div className="wrapper__courses__container">
+          <h2 className="courses__title">Все учебные курсы:</h2>
+          <div className="courses__row">
+            <div className="courses__col">
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+            </div>
+            <div className="courses__col">
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+              <div>> Телекоммуникационные технологии</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Start;
