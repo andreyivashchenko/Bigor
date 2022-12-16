3a85f42bc32c2791c78f03e6bfb0bdd6082e9711
andreyivashchenko <99690245+andreyivashchenko@users.noreply.github.com>
2022-12-16 03:06:46 +0300

    сделан поиск по курсам

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Input.module.scss";
+import { Link } from "react-router-dom";
 
-const Input = () => {
+const Input = ({ courses, Click }) => {
+  const [active, setActive] = useState(false);
+  const [value, setValue] = useState("");
+  const all = [...courses.firstList, ...courses.secondList];
+  const filtered = all.filter((course) => {
+    return course.name.toLowerCase().includes(value.toLowerCase());
+  });
+
   return (
-    <form className={styles.search}>
-      <div className={styles.search__block}>
-        <button className={styles.search__btn} type="submit"></button>
-        <input
-          type="text"
-          placeholder="Поиск..."
-          className={styles.search__input}
-        />
-        <div className={styles.search__select__wrapper}>
-          <select name="" id="" className={styles.search__select}>
-            <option selected value="">
-              Курсы
-            </option>
-            <option value="">Тезаурус</option>
-          </select>
-          <div className={styles.search__select__wrapper__arr}></div>
+    <div
+      className={active ? styles.modal__active : styles.modal}
+      onClick={() => setActive(false)}
+    >
+      <form className={styles.search} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.search__block}>
+          <button
+            className={styles.search__btn}
+            type="submit"
+            disabled
+          ></button>
+          <input
+            type="text"
+            placeholder="Поиск..."
+            className={styles.search__input}
+            onClick={() => {
+              setActive(true);
+            }}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <div className={styles.search__select__wrapper}>
+            <select name="" id="" className={styles.search__select}>
+              <option selected value="">
+                Курсы
+              </option>
+              <option value="">Тезаурус</option>
+            </select>
+            <div className={styles.search__select__wrapper__arr}></div>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      <ul className={styles.autocomplete}>
+        {value
+          ? filtered.map((course) => (
+              <li className={styles.autocomplete__item}>
+                <Link
+                  className={styles.link}
+                  to={course.link}
+                  onClick={() => Click(course)}
+                >
+                  {course.name}
+                </Link>
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
   );
 };
 
